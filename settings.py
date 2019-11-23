3e7346800d7e16f0741d81f96aa374bbb7c74880
Nupur Yadav <nupur.yadav@sjsu.edu>
2019-11-23 15:41:11 -0800

    adding changes

--- settings.py
+++ settings.py
@@ -1,8 +1,9 @@
 conf = {
     "mongo_url" : "mongodb+srv://lokv007:lokesh99@mongodb-2fhcm.mongodb.net/admin", 
-    "host_url": "https://lmp.nupursjsu.net",
+    "host_url": "http://lmp.nupursjsu.net",
+    
     #"host_url": "http://127.0.0.1:8080",
-    "host" : "127.0.0.1",
-    "port" : 8080
+    "host" : "0.0.0.0",
+    "port" : 80
     #"host_url": "http://localhost",
 }
