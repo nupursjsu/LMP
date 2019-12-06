beafa2b989a30ff7fb5d01ac6ae0570362ce69c5
Nupur Yadav <nupur.yadav@sjsu.edu>
2019-12-06 12:55:46 -0800

    adding new test cases

--- functionsTest.py
+++ functionsTest.py
@@ -32,5 +32,48 @@ class functionsTest(unittest.TestCase):
             if key in user_details:
                 self.assertEqual(user_details[key], output[key])
 
+
+    def test_display_bookdetails(self):
+        book_details = {'userId': 'user1', 'firstName': 'myFirstName', 'lastName': 'myLastName', 'age': 20}
+        userId = "user1"
+
+        details = go(display_userdetails, userId, self.testConf)
+        query = OpQuery({"userId": "user1"}, namespace="Books.myapp_user", fields={"_id": False})
+        request = self.server.receives(query)
+        request.reply(book_details)
+
+        output = details()
+        for key in output:
+            if key in book_details:
+                self.assertEqual(book_details[key], output[key])
+
+    def test_update_bookdetails(self):
+        book_details = {'userId': 'user1', 'firstName': 'myFirstName', 'lastName': 'myLastName', 'age': 20}
+        userId = "user1"
+
+        details = go(display_userdetails, userId, self.testConf)
+        query = OpQuery({"userId": "user1"}, namespace="Books.myapp_user", fields={"_id": False})
+        request = self.server.receives(query)
+        request.reply(book_details)
+
+        output = details()
+        for key in output:
+            if key in book_details:
+                self.assertEqual(book_details[key], output[key])
+
+    def test_update_userdetails(self):
+        user_details = {'userId': 'user1', 'firstName': 'myFirstName', 'lastName': 'myLastName', 'age': 20}
+        userId = "user1"
+
+        details = go(display_userdetails, userId, self.testConf)
+        query = OpQuery({"userId": "user1"}, namespace="Books.myapp_user", fields={"_id": False})
+        request = self.server.receives(query)
+        request.reply(user_details)
+
+        output = details()
+        for key in output:
+            if key in user_details:
+                self.assertEqual(user_details[key], output[key])
+
 if __name__ == '__main__':
     unittest.main()
